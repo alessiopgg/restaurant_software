bced60ebb0b1fd4643312709026521c96350a028
Alessio <alepoggejakad@gmail.com>
2024-02-17 18:20:45 +0100

    Create new classes: Admin, Customer, Main, Reservation, Table.

---
+++ restaurant_java_project/src/restaurant_java_project/Admin.java
@@ -0,0 +1,16 @@
+package restaurant_java_project;
+
+import java.util.ArrayList;
+
+public class Admin {
+	ArrayList<Reservation>reservationList=new ArrayList<>();
+	ArrayList<Table>tableList=new ArrayList<>();
+	
+	
+	public Admin() {
+		reservationList=new ArrayList<>();
+		tableList=new ArrayList<>();
+	}
+	
+	
+}
---
+++ restaurant_java_project/src/restaurant_java_project/Customer.java
@@ -0,0 +1,18 @@
+package restaurant_java_project;
+
+import java.time.LocalDateTime;
+
+public class Customer {
+	private Reservation reservation;
+	
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Reservation createReservation(String n,String pn,Integer np,LocalDateTime d,String sr) {
+		
+		return reservation;
+			
+	}
+
+}
